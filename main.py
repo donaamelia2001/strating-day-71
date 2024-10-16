5c30cce886e58f6643a8245587b827c103656503
donaamelia2001 <donaamelia.2001@gmail.com>
2024-10-16 18:41:20 +0700

    importing os package

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
